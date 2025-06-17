37d0b99f8df4f04220c7a305e354a5d0fa2cd733
hanseklevstad <hanserling.klevstad@bym.oslo.kommune.no>
2025-06-17 18:09:59 +0200

    fikser ting som knekte (img-stuff)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), image()],
+  integrations: [mdx()],
 });
