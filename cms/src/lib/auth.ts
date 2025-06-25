6ee7d115858d8cf40081abc82d6273f0cb7dce31
hanseklevstad <hanserling.klevstad@bym.oslo.kommune.no>
2025-06-25 15:28:00 +0200

    fix auth: Putt redirect-urls i env-filer

--- cms/src/lib/auth.ts
+++ cms/src/lib/auth.ts
@@ -1,11 +1,10 @@
-
 import { UserManager } from "oidc-client-ts";
 
 const oidcConfig = {
   authority: "https://sandbox-oslo.onelogin.com/oidc/2", // OneLogin issuer
   client_id: "4a1e0220-33ec-013e-3f9e-5f10d2e0632e38599",
-  redirect_uri: "http://localhost:5173/callback", // adjust for prod
-  post_logout_redirect_uri: "http://localhost:5173/",
+  redirect_uri: import.meta.env.VITE_ONELOGIN_REDIRECT_URI, // adjust for prod
+  post_logout_redirect_uri: import.meta.env.VITE_POST_LOGOUT_REDIRECT_URI,
   response_type: "code",
   scope: "openid profile email",
   automaticSilentRenew: true,
